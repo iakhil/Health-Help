35fe5517aeca0f60a3a45c65d9c478b346b606fc
unknown <akhilkashyap16@gmail.com>
2023-08-20 14:40:56 +0530

    Add form to submit data for a new entry

--- sanic-react-app/src/Create.js
+++ sanic-react-app/src/Create.js
@@ -4,15 +4,28 @@ import axios from 'axios';
 
 function Create() {
 
-    
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(e);
+
+        try {
+            const response = await axios.post(`http://localhost:8000/create`, {name, description});
+            console.log("Data submitted successfully.", response.data);
+        } catch(error){
+            console.error('An error occurred:', error);
+        }
+    }
+
     return (
 
         <div className="create-form">
-        <form action="/create" method="post">
-           <input type="text" name="name" placeholder='Name' />
-           <input type="description" name="descrption" placeholder='Description' />
+        <form onSubmit={handleSubmit}>
+           <input type="text" name="name" value={name} placeholder='Name' onChange={(e) => setName(e.target.value)} />
+           <input type="description" name="descrption" value={description} placeholder='Description' onChange={(e) => setDescription(e.target.value)} />
 
-           <button>Submit</button>
+           <button type="Submit">Submit</button>
     </form>
 
     </div>
