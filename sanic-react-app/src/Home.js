43b1c2bac0d342a930dd6f8e3322d4a2a8828fe6
unknown <akhilkashyap16@gmail.com>
2023-08-21 23:45:29 +0530

    Create home page

---
+++ sanic-react-app/src/Home.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import './App.css';
+import axios from 'axios';
+import Create from './Create';
+import Read from './Read';
+import Update from './Update';
+import Delete from './Delete';
+
+function Home(){
+
+
+
+
+
+
+
+    return (
+
+        <div>
+
+            
+       <Create/>
+       <Read/>
+       <Update/>
+
+        <Delete/>
+
+
+        </div>
+
+
+
+
+    )
+}
